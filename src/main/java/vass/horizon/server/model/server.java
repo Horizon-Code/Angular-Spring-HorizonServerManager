ddcab1456889d00ebe13abc8b1acfe792c00b258
Horizon-code <rubenpasamarsanchez@gmail.com>
2022-07-19 20:40:49 +0200

    first commit

---
+++ src/main/java/vass/horizon/server/model/server.java
@@ -0,0 +1,32 @@
+package vass.horizon.server.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import vass.horizon.server.enumeration.Status;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class server {
+    @Id
+    @GeneratedValue(strategy=AUTO)
+    private Long id;
+    @Column(unique = true)
+    @NotEmpty(message= "IP Adress cannot be empty or null")
+    private String ipAdress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+}
